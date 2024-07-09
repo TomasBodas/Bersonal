1e91056f706b3365fc35c18a477d2b8351a40661
JuanCruz17 <48994507+JuanCruz17@users.noreply.github.com>
2024-07-09 18:59:25 -0300

    Página Bitácora

---
+++ DAL/DAL_Log.cs
@@ -0,0 +1,47 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.Models;
+using System.Runtime.Remoting.Messaging;
+using System.Collections;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace DAL
+{
+    public class DAL_Log
+    {
+        private static readonly string CONNECTION_STRING = DataBaseServices.getConnectionString();
+        List<Log> listlog = new List<Log>();
+        public List<Log> getLog()
+        {
+            using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+            {
+
+                string select = "SELECT b.id, b.fecha, u.email, b.modulo, b.descripcion FROM bitacora b JOIN usuario u ON b.fk_usuario_bitacora = u.id";
+                SqlCommand command = new SqlCommand(select, connection);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Log log = new Log
+                        {
+                             id = reader.GetInt32(0),
+                             Fecha = reader.GetDateTime(1),
+                             User = reader.GetString(2),
+                             Modulo = reader.GetString(3),
+                             Descripcion = reader.GetString(4)       
+                        };
+                        listlog.Add(log);                
+                            
+                    }  
+                } 
+            }
+            return listlog;
+        }
+    }
+}
---
+++ UAIDesarrolloArquitectura/Controllers/BitacoraController.cs
@@ -0,0 +1,36 @@
+﻿using DAL;
+using Services;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows;
+using UAIDesarrolloArquitectura.Models.ViewModel;
+
+
+namespace UAIDesarrolloArquitectura.Controllers
+{
+    public class BitacoraController : Controller
+    {
+        DAL_Log dAL_Log = new DAL_Log();
+        public ActionResult Bitacora()
+        {
+            List<Log> list = dAL_Log.getLog();
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult Bitacora(DateTime Fechainicio, DateTime Fechafinal)
+        {
+            List<Log> list = dAL_Log.getLog();
+            if (Fechainicio != null || Fechafinal != null)
+            {
+                List<Log> filteredList = list.Where(p => p.Fecha >= Fechainicio && p.Fecha <= Fechafinal).ToList();
+                return View(filteredList);
+            }
+            else return View(list);    
+        }
+    }
+}
